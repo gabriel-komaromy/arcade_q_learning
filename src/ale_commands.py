67f1ad157d85c849f3e4e9aa72d6a349baa596b7
g-ewing <gre5@case.edu>
2016-12-04 15:58:23 -0500

    built q-network
    Signed-off-by: g-ewing <gre5@case.edu>

--- src/ale_commands.py
+++ src/ale_commands.py
@@ -4,7 +4,10 @@ from scipy import misc
 import numpy as np
 
 breakout = ale.ALEInterface()
-breakout.loadROM('../Arcade-Learning-Environment-0.5.1/roms/Breakout.bin')
+path = '/Users/Gabe/classes/current/600/project/Arcade-Learning-Environment' + \
+    '-0.5.1/roms/Breakout.bin'
+
+breakout.loadROM(path)
 # breakout.getLegalActionSet()
 
 """This is the one I probably want to use, it's only the actions that have
@@ -41,6 +44,15 @@ def bias_variable(shape):
     return tf.Variable(initial)
 
 
+def conv3d(x, W, stride_size):
+    return tf.nn.conv3d(
+        x,
+        W,
+        strides=[1, 1, stride_size, stride_size, 1],
+        padding='SAME',
+        )
+
+
 def new_frame():
     breakout.act(1)
     grayscale_frame = breakout.getScreenGrayscale()
@@ -56,3 +68,27 @@ def frame_stack():
     tf_frames = tf.reshape(frames_array, [-1, 4, 108, 84, 1])
     float_frames = tf.to_float(tf_frames)
     return float_frames
+
+
+trial_stack = frame_stack()
+W_conv1 = weight_variable([1, 8, 8, 1, 16])
+b_conv1 = bias_variable([16])
+h_conv1 = tf.nn.relu(conv3d(trial_stack, W_conv1, 4) + b_conv1)
+
+W_conv2 = weight_variable([1, 4, 4, 16, 32])
+b_conv2 = bias_variable([32])
+
+h_conv2 = tf.nn.relu(conv3d(h_conv1, W_conv2, 2) + b_conv2)
+h_conv2_flat = tf.reshape(h_conv2, [-1, 4 * 14 * 11 * 32])
+
+W_fc1 = weight_variable([4 * 14 * 11 * 32, 256])
+b_fc1 = bias_variable([256])
+
+h_fc1 = tf.nn.relu(tf.matmul(h_conv2_flat, W_fc1) + b_fc1)
+
+num_actions = 4
+
+W_fc2 = weight_variable([256, num_actions])
+b_fc2 = bias_variable([num_actions])
+
+y_conv = tf.matmul(h_fc1, W_fc2) + b_fc2
